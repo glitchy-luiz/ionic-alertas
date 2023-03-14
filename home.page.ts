bd35c824b5492f32e9173048d9df92721cbc826e
Luiz Fernando <84513178+glitchy-luiz@users.noreply.github.com>
2023-03-14 13:06:26 -0300

    Add files via upload

---
+++ home.page.ts
@@ -0,0 +1,27 @@
+/* eslint-disable prefer-const */
+/* eslint-disable @typescript-eslint/ban-types */
+import { Component } from '@angular/core';
+import { AlertController } from '@ionic/angular';
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+  constructor(public alertController: AlertController) {}
+  async exibirAlerta(mensagem: string) {
+    const alert = await this.alertController.create({
+      cssClass: 'my-custom-class',
+      header: 'Título',
+      subHeader: 'Subtitulo',
+      message: mensagem,
+      buttons: ['OK'],
+    });
+    await alert.present();
+  }
+  verificaSelecionado(valor: any) {
+    let mensagem: string;
+    mensagem = valor.detail.value;
+    this.exibirAlerta(mensagem);
+  }
+}
